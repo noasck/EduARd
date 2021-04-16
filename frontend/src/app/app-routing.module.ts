63a2084a7b302ed6a21aa2d4437a63ba2bba0906
supruniuk <juliasupruniuk23@gmail.com>
2021-04-16 19:46:04 +0300

    wrong

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,7 +1,27 @@
 import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { Routes, RouterModule } from '@angular/router';
+import { AdminComponent } from './admin/admin.component';
 
-const routes: Routes = [];
+import { LoginPageComponent } from './admin/login-page/login-page.component';
+import { PupsCreateComponent } from './admin/pups/pups-create/pups-create.component';
+import { PupsReadComponent } from './admin/pups/pups-read/pups-read.component';
+import { LandingPageComponent } from './landing-page/landing-page.component';
+
+const routes: Routes = [
+  {
+    path: '', component: LandingPageComponent
+  },
+  {
+    path: 'admin', component: AdminComponent, children: [
+      {path: 'pups', component: PupsReadComponent, children: [
+        {path: 'create', component: PupsCreateComponent}
+      ]}
+    ]
+  },
+  {
+    path: 'login', pathMatch: 'full', component: LoginPageComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
