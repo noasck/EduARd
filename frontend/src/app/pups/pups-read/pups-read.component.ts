44b3cfc831278b766ba9fa4041a1fbe391069249
supruniuk <juliasupruniuk23@gmail.com>
2021-04-17 05:03:21 +0300

    delete pup

--- frontend/src/app/pups/pups-read/pups-read.component.ts
+++ frontend/src/app/pups/pups-read/pups-read.component.ts
@@ -7,6 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PupsReadComponent implements OnInit {
   src: string = ""
+  deletePupId = ""
   constructor() { }
 
   ngOnInit(): void {
